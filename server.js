c7ef1af88098ded0cffc1cb2c7fed4d90c1df248
Jerred Almasco <jtaalmasco@gmail.com>
2017-04-04 16:16:30 +0800

    connection setup

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-var express = require('express');
-var app = express();
+var express = require('express'); //call express
+var app = express(); //define our app  using express
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var Bear     = require('./app/models/bear');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+
+//configure app to use bodyParser()
+//this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080; //set our port
+
+var router = express.Router(); 
+
+// ROUTES FOR OUR API
+//
+router.get('/', function(req, res){
+    res.json({message: 'hooray! welcome to our api'});
+});
+//more routes for our API will happen here
+//REGISTER OUR ROUTES ----------------
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
