8b2c2775eb4c86c0d91cd119714a49b79c4e5c6c
Jerred Almasco <jtaalmasco@gmail.com>
2017-04-06 11:11:45 +0800

    revision of server.js

--- server.js
+++ server.js
@@ -1,34 +1,125 @@
-var express = require('express'); //call express
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
 var bodyParser = require('body-parser');
-var app = express(); //define our app  using express
+var app        = express();
+var morgan     = require('morgan');
 
-var mongoose   = require('mongoose');
-var Bear     = require('./app/models/bear');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+// configure app
+app.use(morgan('dev')); // log requests to the console
 
-//configure app to use bodyParser()
-//this will let us get the data from a POST
-app.use(bodyParser.urlencoded({extended: true}));
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; //set our port
+var port     = process.env.PORT || 8080; // set our port
 
-var router = express.Router(); 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+var Bear     = require('./app/models/bear');
 
 // ROUTES FOR OUR API
-//
-router.get('/', function(req, res){
-    res.json({message: 'hooray! welcome to our api'});
+// =============================================================================
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
 });
-//more routes for our API will happen here
-//REGISTER OUR ROUTES ----------------
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+	// create a bear (accessed at POST http://localhost:8080/bears)
+	.post(function(req, res) {
+		
+		var bear = new Bear();		// create a new instance of the Bear model
+		bear.name = req.body.name;  // set the bears name (comes from the request)
+
+		bear.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Bear created!' });
+		});
+
+		
+	})
+
+	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	.get(function(req, res) {
+		Bear.find(function(err, bears) {
+			if (err)
+				res.send(err);
+
+			res.json(bears);
+		});
+	});
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+/*
+	
+*/
+router.route('/bears/:bear_id')
+
+	// get the bear with that id
+	.get(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+			if (err)
+				res.send(err);
+			res.json(bear);
+		});
+	})
+    /*
+	
+    */
+	// update the bear with this id
+	.put(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+
+			if (err)
+				res.send(err);
+
+			bear.name = req.body.name;
+			bear.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Bear updated!' });
+			});
+
+		});
+	})
+	// delete the bear with this id
+	.delete(function(req, res) {
+		Bear.remove({
+			_id: req.params.bear_id
+		}, function(err, bear) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+// REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
 
-router.route('/bears').post(function(){
-    var bear = new Bear();
-})
-
+// START THE SERVER
+// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
-
-var express = require('express');
